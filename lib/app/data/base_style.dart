bc96c7077abbf57b8ef7448b28bf1395ede6be73
{2631315464@qq.com} <chenrongqi@njbandou.com>
2022-11-07 18:52:27 +0800

    智慧入校入口、首页

--- lib/app/data/base_style.dart
+++ lib/app/data/base_style.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wit_niit/app/data/school_theme_data.dart';
 
 class BaseStyle {
   /// 字号
+  static TextStyle fs14bold =
+      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.bold);
   static TextStyle fs16 = TextStyle(fontSize: 16.sp);
-  static TextStyle fs26 = TextStyle(fontSize: 26.sp, fontWeight: FontWeight.bold);
+  static TextStyle fs16bold =
+      TextStyle(fontSize: 16.sp, fontWeight: FontWeight.bold);
+  static TextStyle fs26 =
+      TextStyle(fontSize: 26.sp, fontWeight: FontWeight.bold);
 
   /// 样式
   static TextStyle wdStyle = TextStyle(color: Colors.pink, fontSize: 30.sp);
+
+  /// 顶部导航字体样式(浅色
+  static TextStyle topStyle = TextStyle(color: Colors.white, fontSize: 20.sp);
+
+  /// 灰色中号字体
+  static TextStyle grayContentStyle =
+      TextStyle(color: Colors.grey.shade700, fontSize: 16.sp);
+
+  /// 灰色小号字体
+  static TextStyle graySmallStyle =
+      TextStyle(color: Colors.grey.shade500, fontSize: 14.sp);
+
+  /// 黑色中号字体
+  static TextStyle contentStyle =
+      TextStyle(color: Colors.black, fontSize: 16.sp);
+
+  /// 黑色小号字体
+  static TextStyle smallStyle = TextStyle(color: Colors.black, fontSize: 12.sp);
+
+  /// 智慧入校主题色中号字体
+  static TextStyle schoolContentStyle =
+      TextStyle(color: SchoolConfig.primaryColor, fontSize: 16.sp);
+
+  /// 智慧入校主题色小号字体
+  static TextStyle schoolSmallStyle =
+      TextStyle(color: SchoolConfig.primaryColor, fontSize: 14.sp);
 }
--- lib/app/modules/bench/views/bench_view.dart
+++ lib/app/modules/bench/views/bench_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:wit_niit/app/component/my_Botton.dart';
 
 import '../controllers/bench_controller.dart';
+import 'enter_school_view.dart';
 
 class BenchView extends GetView<BenchController> {
   const BenchView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +16,15 @@ class BenchView extends GetView<BenchController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'BenchView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          children: [
+            MyButton(
+              onTap: () {
+                Get.to(EnterSchoolView());
+              },
+              title: '去智慧入校',
+            ),
+          ],
         ),
       ),
     );
