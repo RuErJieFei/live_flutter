32c4297e612ea502da8045638023b42ff44d3bcf
zhangke <1826213722@qq.com>
2022-11-17 23:47:06 +0800

    性别选择、个人状态设置静态页面

--- lib/app/modules/personal/controllers/personal_gender_controller.dart
+++ lib/app/modules/personal/controllers/personal_gender_controller.dart
@@ -1,12 +1,29 @@
+import 'package:flustars/flustars.dart';
 import 'package:get/get.dart';
+import 'package:wit_niit/app/modules/login/model/user_model.dart';
+import 'package:wit_niit/app/modules/personal/model/personal_gender_model.dart';
 
 class PersonalGenderController extends GetxController {
-  //TODO: Implement PersonalGenderController
+  final gender = false.obs;
+  var genderList = <PersonalGender>[].obs;
+  UserModel? user = SpUtil.getObj(
+      "user", (v) => UserModel.fromJson(v as Map<String, dynamic>));
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    genderList.add(
+        PersonalGender(genderName: '男', genderValue: 'M', isSelect: false));
+    genderList.add(
+        PersonalGender(genderName: '女', genderValue: 'F', isSelect: false));
+    genderList.add(
+        PersonalGender(genderName: '未知', genderValue: 'U', isSelect: false));
+
+    genderList.forEach((element) {
+      if (user?.gender == element.genderValue) {
+        element.isSelect = element.isSelect == true ? false : true;
+      }
+    });
   }
 
   @override
@@ -19,5 +36,14 @@ class PersonalGenderController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void changeGender(PersonalGender personalGender) {
+    genderList.forEach((element) {
+      if (personalGender.genderValue == element.genderValue) {
+        element.isSelect = true;
+      } else {
+        element.isSelect = false;
+      }
+    });
+    genderList.refresh();
+  }
 }
--- lib/app/modules/personal/views/personal_gender_view.dart
+++ lib/app/modules/personal/views/personal_gender_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:wit_niit/app/modules/personal/controllers/personal_gender_controller.dart';
 
 class PersonalGenderView extends GetView<PersonalGenderController> {
   const PersonalGenderView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,12 +13,48 @@ class PersonalGenderView extends GetView<PersonalGenderController> {
         title: const Text('性别'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'PersonalGenderView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: Container(
+          // child: Column(
+          //   children: <Widget>[
+          //     Obx(() {
+          //       return ListTile(
+          //         leading: Text('男'),
+          //         trailing: controller.gender == true
+          //             ? Icon(Icons.check)
+          //             : Container(
+          //                 width: 1.w,
+          //               ),
+          //         onTap: () => controller.changeGender(),
+          //       );
+          //     }),
+          //     ListTile(
+          //       leading: Text('女'),
+          //       trailing: Icon(Icons.check),
+          //       onTap: () {},
+          //     ),
+          //     ListTile(
+          //       leading: Text('未知'),
+          //       trailing: Icon(Icons.check),
+          //       onTap: () {},
+          //     ),
+          //   ],
+          // ),
+          child: Obx(() {
+        return ListView.builder(
+            itemCount: controller.genderList.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Text(controller.genderList[index].genderName),
+                trailing: controller.genderList[index].isSelect == true
+                    ? Icon(Icons.check)
+                    : Container(
+                        width: 1.w,
+                      ),
+                onTap: () =>
+                    controller.changeGender(controller.genderList[index]),
+              );
+            });
+      })),
     );
   }
 }
